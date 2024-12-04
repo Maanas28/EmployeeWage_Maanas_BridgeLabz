8d1c9b9630db288bb7f50a3d6e11b1bb2596c0c6
Maanas28 <maanaskanwar88@gmail.com>
2024-12-04 14:48:21 +0530

    Added the UseCase-2 functionality

--- Project-Files/script.js
+++ Project-Files/script.js
@@ -10,4 +10,37 @@ function attendanceChecker() {
 
     return status;
 }
-attendanceChecker();
+function calculateDailyWage() {
+    const HOURLY_WAGE_RATE = 20; 
+    var randomValue = Math.random();
+    var employmentState;
+    var dailyWage = 0;
+
+    // Determine employment type
+    if (randomValue > 0 && randomValue <= 0.3) {
+        employmentState = "FULL_TIME";
+    } else if (randomValue > 0.3 && randomValue <= 0.6) {
+        employmentState = "PART_TIME";
+    } else {
+        employmentState = "NO_TIME";
+    }
+
+    // Calculate daily wage based on employment state
+    switch (employmentState) {
+        case "FULL_TIME":
+            dailyWage = 8 * HOURLY_WAGE_RATE; 
+            console.log("Full-Time: Daily Wage is $" + dailyWage);
+            break;
+
+        case "PART_TIME":
+            dailyWage = 4 * HOURLY_WAGE_RATE; 
+            console.log("Part-Time: Daily Wage is $" + dailyWage);
+            break;
+
+        default:
+            console.log("No Work: Daily Wage is $0");
+    }
+}
+
+calculateDailyWage();
+
